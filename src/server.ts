98c838b581a543861e4d760bdcd5a73590133411
Liam Gérard <l.gerard@students.ephec.be>
2026-02-09 15:54:26 +0100

    jusqu'à l'exercice 6.2

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,24 @@
-function greet(name: string): string {
-    return `Hello, ${name}!`;
-}
-console.log(greet("World"));
+import express from 'express';
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Bienvenue sur ma route API');
+});
+app.listen(port, () => {
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
+
+const etudiants = [
+{ id: 1, nom: "Dupont", prenom: "Jean" },
+{ id: 2, nom: "Martin", prenom: "Sophie" },
+{ id: 3, nom: "Doe", prenom: "John" },
+];
+
+app.get('/api/data', (req: Request, res: Response) => {
+    res.json(etudiants);
+});
