8a30b45f332553f3d7a8bf77963f1acad1de8fee
Liam Gérard <l.gerard@students.ephec.be>
2026-02-09 16:53:46 +0100

    fin du tp^01, assez complexe

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express from 'express';
 import express, { Request, Response } from 'express';
+import userRoutes from './routes/userRoutes';
 
 
 const app = express();
@@ -8,12 +8,15 @@ const port = 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Bienvenue sur ma route API');
 });
+
+app.use('/api', userRoutes);
+
 app.listen(port, () => {
     console.log(`Serveur lancé sur http://localhost:${port}`);
 });
 
 
-const etudiants = [
+/*const etudiants = [
 { id: 1, nom: "Dupont", prenom: "Jean" },
 { id: 2, nom: "Martin", prenom: "Sophie" },
 { id: 3, nom: "Doe", prenom: "John" },
@@ -22,3 +25,12 @@ const etudiants = [
 app.get('/api/data', (req: Request, res: Response) => {
     res.json(etudiants);
 });
+
+app.get('/api/hello/:name', (req: Request, res: Response) => {
+    const name = req.params.name;
+    const reponse = {
+        message: `Bonjour ${name}`,
+        timestamp: new Date().toISOString(),
+    };
+    res.json(reponse);
+});*/
