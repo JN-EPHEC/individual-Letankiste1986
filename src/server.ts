76e4ba323a8a53a3e64b30766d54ba4ab3a6404a
Liam Gérard <l.gerard@students.ephec.be>
2026-02-13 11:41:16 +0100

    CRUD terminé et fonctionnel

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import './models/User';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Bienvenue sur ma route API');
 });
